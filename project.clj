31b290f1f997548882608c5a772f49bc7b2ccad0
Phong Ho <pho@Phong-Hos-MacBook-Pro-3.local>
2012-04-09 14:11:31 -0700

    upgrading caribou dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lein-caribou "1.0.0-SNAPSHOT"
   :description "Caribou Provisioning Tool"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [antler/caribou-core "0.4.0-SNAPSHOT"]]
+                 [antler/caribou-core "0.4.1-SNAPSHOT"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]
                      [lein-clojars "0.6.0"]]
   :eval-in-leiningen true)
