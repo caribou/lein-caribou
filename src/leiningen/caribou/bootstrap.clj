8161ceee68cea243765e1e91966c777332147efe
ryan spangler <ryan.spangler@gmail.com>
2012-04-27 12:22:57 -0700

    got bootstrap working again

--- src/leiningen/caribou/bootstrap.clj
+++ src/leiningen/caribou/bootstrap.clj
@@ -1,11 +1,12 @@
 (ns leiningen.caribou.bootstrap
+  (:use caribou.debug)
   (:require [caribou.config :as config]
             [caribou.tasks.bootstrap :as bootstrap]))
 
 (defn bootstrap
   [_ config-file]
   (let [config (config/read-config config-file)
-        database (-> config :database)]
+        database (debug (config/assoc-subname (config :database)))]
     (bootstrap/bootstrap database)))
 
 ;; (defn bootstrap-all
