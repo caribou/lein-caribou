66c108bd69dc9cbbe79ce0a247f76a6b2822b38b
eric feigner <eric@weareinstrument.com>
2012-05-02 14:20:09 -0700

    adding snapshot / release repos for jar publishin'

--- project.clj
+++ project.clj
@@ -13,4 +13,8 @@
   :dev-dependencies [[lein-eclipse "1.0.0"]
                      [lein-clojars "0.6.0"]]
   :eval-in-leiningen true
-  :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"])
+  :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
+  :repositories {"snapshots" {:url "http://battlecat:8080/nexus/content/repositories/snapshots" 
+                              :username "deployment" :password "deployment"}
+                 "releases"  {:url "http://battlecat:8080/nexus/content/repositories/releases" 
+                              :username "deployment" :password "deployment"}})
