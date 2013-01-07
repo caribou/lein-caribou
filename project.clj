4fc3e796971677a60d60dc9e09cb2010c40ad597
Justin Glenn Smith <noisesmith@gmail.com>
2013-01-07 13:27:35 -0800

    "version bump"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/lein-caribou "1.5.0"
+(defproject antler/lein-caribou "1.5.1"
   :description "Caribou Provisioning Tool"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [leiningen-core "2.0.0-preview3"]
