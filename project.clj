54e651942fb46453d849cb3b25dded6e97de42b6
Justin Glenn Smith <noisesmith@gmail.com>
2013-01-07 13:24:51 -0800

    latest caribou core version

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [com.cemerick/pomegranate "0.0.11"]
                  [antler/zippix "0.1.0"]
                  [antler/commons-io "2.2.0"]
-                 [antler/caribou-core "0.7.2"]]
+                 [antler/caribou-core "0.7.17"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]
                      [lein-clojars "0.6.0"]]
   :eval-in-leiningen true
