0c976bb6a20eb1d4f8953cba6561ec17b7afe09f
Justin Glenn Smith <noisesmith@gmail.com>
2013-03-04 13:08:41 -0800

    version updates

--- project.clj
+++ project.clj
@@ -1,17 +1,24 @@
-(defproject antler/lein-caribou "1.7.0"
+(defproject antler/lein-caribou "2.0.0"
   :description "Caribou Provisioning Tool"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [leiningen-core "2.0.0-preview3"]
-                 [ring "1.1.6"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [leiningen-core "2.0.0"]
+                 [ring "1.1.0"
                   :exclusions [org.clojure/clojure
                                clj-stacktrace
-                               hiccup]]
+                               hiccup
+                               commons-io]]
                  [lein-ring "0.7.1"]
                  ;; [ring/ring-servlet "1.1.0"]
-                 [ring/ring-jetty-adapter "1.1.0"]
-                 [com.cemerick/pomegranate "0.0.11"]
+                 ;; [ring/ring-jetty-adapter "1.1.0"
+                 ;;  :exclusions []]
+                 ;; [com.cemerick/pomegranate "0.0.11"]
                  [antler/zippix "0.1.0"]
                  [antler/commons-io "2.2.0"]
-                 [antler/caribou-core "0.8.8"]]
+                 [antler/caribou-core "0.9.0"
+                  :exclusions [leiningen-core
+                               org.jsoup/jsoup
+                               org.apache.httpcomponents/httpclient]]]
+  :dev-dependencies [[lein-eclipse "1.0.0"]
+                     [lein-clojars "0.6.0"]]
   :eval-in-leiningen true
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"])
