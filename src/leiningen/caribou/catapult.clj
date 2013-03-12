984f1df5d20ee3145174025e386e8b45be1e0872
ryan spangler <ryan.spangler@gmail.com>
2013-03-12 15:55:54 -0700

    catapult in action

--- src/leiningen/caribou/catapult.clj
+++ src/leiningen/caribou/catapult.clj
@@ -1,8 +1,13 @@
 (ns leiningen.caribou.catapult
-  (:require [caribou.config :as config]
-            [caribou.asset :as asset]))
+  (:require [leiningen.core.eval :as eval]
+            [leiningen.caribou.server :as server]))
 
 (defn catapult
   [prj dir bucket prefix]
-  (config/init)
-  (asset/migrate-dir-to-s3 dir bucket prefix))
+  (println "Catapulting" dir "to the" bucket "s3 bucket under" prefix)
+  (eval/eval-in-project
+   prj
+   `(do
+      (caribou.config/init)
+      (caribou.asset/migrate-dir-to-s3 '~dir '~bucket '~prefix))
+   (server/load-namespaces 'caribou.config 'caribou.asset)))
