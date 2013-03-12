123f9b8cef801dc17de7bcfcf69ea12a3456b565
ryan spangler <ryan.spangler@gmail.com>
2013-03-12 15:56:11 -0700

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/lein-caribou "2.0.0"
+(defproject antler/lein-caribou "2.0.1"
   :description "Caribou Provisioning Tool"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [leiningen-core "2.0.0"]
