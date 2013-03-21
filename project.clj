95a7f937e115362c87a5eca19bd52aabbd51661a
Kyle Dawkins <kyle.dawkins@weareinstrument.com>
2013-03-20 17:53:42 -0700

    Version bump, etc.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/lein-caribou "2.0.6"
+(defproject antler/lein-caribou "2.0.7"
   :description "Caribou Provisioning Tool"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [leiningen-core "2.0.0"]
