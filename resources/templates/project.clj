ac58b1a4f81537d87ba66418cf9ea68cb443221f
ryan spangler <ryan.spangler@gmail.com>
2013-05-07 14:27:18 -0700

    removed a bunch of unnecessary cruft

--- resources/templates/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject $project$ "1.0.0-SNAPSHOT"
-  :description "Caribou: $project$"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [antler/caribou-frontend "0.1.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-ring "0.6.3"]
-                     [swank-clojure "1.4.0-SNAPSHOT"]]
-  :ring {:handler caribou-frontend.core/app
-         :servlet-name "$project$-frontend"
-         :init caribou-frontend.core/init})
