60c2e203a772f7e0a5995388398e51e8dfa9e498
ryan spangler <ryan.spangler@gmail.com>
2013-08-16 17:17:30 -0700

    latest resource package

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/lein-caribou "2.2.8"
+(defproject antler/lein-caribou "2.3.0"
   :description "Caribou Provisioning Tool"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [leiningen-core "2.0.0"]
@@ -6,12 +6,12 @@
                   :exclusions [org.clojure/clojure
                                clj-stacktrace
                                hiccup]]
-                 [lein-ring "0.7.1"]
+                 [lein-ring "0.8.2"]
                  ;; [ring/ring-servlet "1.1.0"]
                  [ring/ring-jetty-adapter "1.1.0"]
                  [com.cemerick/pomegranate "0.0.11"]
                  [antler/zippix "0.1.0"]
                  [antler/commons-io "2.2.0"]
-                 [antler/caribou-core "0.11.22"]]
+                 [antler/caribou-core "0.11.28"]]
   :eval-in-leiningen true
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"])
