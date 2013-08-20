e7a00b88339c76cf27da99ab8568b2936db6d1a7
ryan spangler <ryan.spangler@gmail.com>
2013-08-20 14:14:39 -0700

    2.4.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/lein-caribou "2.3.2"
+(defproject antler/lein-caribou "2.4.0"
   :description "Caribou Provisioning Tool"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [leiningen-core "2.0.0"]
@@ -11,6 +11,6 @@
                  [com.cemerick/pomegranate "0.0.11"]
                  [antler/zippix "0.1.0"]
                  [commons-io "2.2"]
-                 [antler/caribou-core "0.11.30"]]
+                 [antler/caribou-core "0.12.0"]]
   :eval-in-leiningen true
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"])
