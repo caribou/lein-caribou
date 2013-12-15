86415776d11129430d6463332729def393b11a75
Alex Poms <alexpoms@gmail.com>
2013-12-15 11:57:26 -0800

    Bracket typo

--- src/leiningen/caribou/catapult.clj
+++ src/leiningen/caribou/catapult.clj
@@ -8,7 +8,7 @@
   (let [[[namespace callback]] (util/retrieve-config-and-args args)]
     (eval/eval-in-project
      prj
-    `(let [config# (caribou.core/init (~callback)]
+    `(let [config# (caribou.core/init (~callback))]
        (caribou.core/with-caribou config#
          (caribou.asset/migrate-dir-to-s3 '~dir)))
     (util/load-namespaces 'caribou.config 'caribou.core 'caribou.asset namespace))))
