411cd4586bee16e640e1c6a34936bce8f42424cb
ryan spangler <ryan.spangler@gmail.com>
2014-03-05 16:31:01 -0800

    latest release

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject caribou/lein-caribou "2.13.2"
+(defproject caribou/lein-caribou "2.14.0"
   :description "Caribou Provisioning Tool"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [leiningen-core "2.0.0"]
+                 [leiningen-core "2.3.4"]
                  [ring "1.2.0"
                   :exclusions [org.clojure/clojure
                                clj-stacktrace
@@ -11,6 +11,6 @@
                  [com.cemerick/pomegranate "0.0.11"]
                  [antler/zippix "0.1.0"]
                  [commons-io "2.2"]
-                 [caribou/caribou-core "0.13.2"]]
+                 [caribou/caribou-core "0.14.0"]]
   :eval-in-leiningen true
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"])
